569d9050de7ac15a500711742a247fa2fa3f9a72
jcsalasdev <jcsalasdev@gmail.com>
2021-12-16 16:03:57 +0800

    edit proj show view

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,7 @@ class Item < ApplicationRecord
 
   def upload_to_s3
     s3 = Aws::S3::Resource.new(region:'us-east-2')
-    # organization_name = Organization.find(current_user.organization_id).name
+    
     obj = s3.bucket("proj-management-app").object("#{1}/#{upload.original_filename}")
     obj.upload_file(upload.path, acl:'public-read')
     self.key = obj.public_url
