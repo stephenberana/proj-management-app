aa1b34fcc312e179623de0032dbac962cc1b45cd
jcsalasdev <jcsalasdev@gmail.com>
2021-12-16 14:26:59 +0800

    remove some links

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,9 +11,6 @@ class ItemsController < ApplicationController
     @item.project_id = params[:project_id]
   end
 
-  # GET /items/1/edit
-  def edit
-  end
 
   # POST /items or /items.json
   def create
@@ -30,24 +27,11 @@ class ItemsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /items/1 or /items/1.json
-  def update
-    respond_to do |format|
-      if @item.update(item_params)
-        format.html { redirect_to @item, notice: "Item was successfully updated." }
-        format.json { render :show, status: :ok, location: @item }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @item.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
   # DELETE /items/1 or /items/1.json
   def destroy
     @item.destroy
     respond_to do |format|
-      format.html { redirect_to dashboard_url, notice: "Item was successfully destroyed." }
+      format.html { redirect_to dasboard_url, notice: "Item was successfully destroyed." }
       format.json { head :no_content }
     end
   end
