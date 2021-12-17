8c656e96801268a754867e85135e6047338f4499
jcsalasdev <jcsalasdev@gmail.com>
2021-12-17 21:25:18 +0800

    project-users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :user_projects
+  put 'user_projects', to: "user_projects#create"
   resources :items
   resources :projects
   resources :organizations
