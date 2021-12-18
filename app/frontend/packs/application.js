494d3ded35097cb168eb84878067a2eefa344c67
jcsalasdev <jcsalasdev@gmail.com>
2021-12-18 19:11:16 +0800

    mod nav

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -2,12 +2,12 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
+import "../js/bootstrap_js_files.js";
 import Rails from "@rails/ujs";
 import Turbolinks from "turbolinks";
 import * as ActiveStorage from "@rails/activestorage";
 import "channels";
-import "../js/bootstrap_js_files.js";
+
 Rails.start();
 Turbolinks.start();
 ActiveStorage.start();
