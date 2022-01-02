de695efc11ff2a80818ab48524a45c1afef02e19
Stephen Berana <stephenberana@gmail.com>
2022-01-03 07:54:52 +0800

    Fixes conflicts with updatd stash with stripe2

--- config/initializers/acts_as_tenant.rb
+++ config/initializers/acts_as_tenant.rb
@@ -1,3 +1,4 @@
 ActsAsTenant.configure do |config|
     config.require_tenant = true
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,8 @@ Rails.application.routes.draw do
 
   devise_for :admins, controllers: {sessions: "sessions", invitations: "invitations"} 
   devise_for :members, controllers: {sessions: "sessions", invitations: "invitations"}
+  devise_for :users, controllers: {sessions: "sessions", registrations: "registrations"}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+match 'plan/edit' => 'organizations#edit', via: :get, as: :edit_plan
 end
